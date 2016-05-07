84d7816be80d3c9b5be33f25e1f88a2a88a6fddd
Christian BODT <christian.bodt@gmail.com>
2016-05-07 23:11:08 +0200

    Implement bold driver -Add log_softmax to avoid log of zeros

--- src/classify/logistic_regression.py
+++ src/classify/logistic_regression.py
@@ -1,10 +1,11 @@
 import numpy as np
 import sys
+import traceback
 print sys.executable
 from itertools import izip
 from gendata import gendata
 from util import softmax
-from classify.util import normalize_rows, column_vector
+from classify.util import normalize_rows, column_vector, log_softmax
 
 np.seterr(all='raise', under="ignore")
 
@@ -13,10 +14,10 @@ def cost_function(W, x, y):
     """
     m = x.shape[0]
     vec = np.dot(x, W);
-    sigmoid_activation = softmax(vec)
+    sigmoid_activation = np.apply_along_axis(log_softmax, 1, vec)
     index = [range(0, np.shape(sigmoid_activation)[0]), y]
     p = sigmoid_activation[index]
-    l = -np.mean(np.log(p ))
+    l = -np.mean(p)
     return l
         
 def compute_gradients(out, y, x):
@@ -62,16 +63,27 @@ def predict(W, x):
     return np.argmax(values, axis=1)
 
 
-def batch_gradient_descent(X, Y, C, l=1, nbiter=10):
+def batch_gradient_descent(X, Y, C, l_start=1, converge_percent=0.01, maxiter=1000, progress_callback=lambda *args: None):
+    """ Batch gradient descent with 'bold driver' learning rate adaptation """
+    
     n = X.shape[1]
     W = np.zeros((n, C)) #n+1, c
-    for i in range(nbiter):
-        print i, cost_function(W, X, Y)
+    l = l_start
+    last_cost  = None
+    for i in range(maxiter):
+        cost = cost_function(W, X, Y)
+        progress_callback(i, cost, l)
         grads = gradients2(W, X, Y)
+        if last_cost is not None and cost is not None:
+            change = (last_cost - cost) / last_cost
+            if change < 0: # bold driver
+                l /= 2.0
+            elif change *100 < converge_percent:
+                return predict(W, X)
+        l = l * 1.1
         W -= grads.T * l
-
+        last_cost = cost
     return predict(W, X)
-
 
 
 if __name__ == '__main__':
@@ -81,7 +93,9 @@ if __name__ == '__main__':
     X, Y, factors = gendata(n=N, m=M, c=C, seed=1)
     #X = np.hstack((X, np.ones((M,1)))) # Add column of 1's
 
-    print Y
-    Y2 = batch_gradient_descent(X, Y, C, l=1, nbiter=200)
+    def print_progress(i, cost, l):
+        print i, cost, l
+
+    Y2 = batch_gradient_descent(X, Y, C, maxiter=1000, progress_callback=print_progress)
     print Y == Y2
-    print
+    print Y
--- src/classify/util.py
+++ src/classify/util.py
@@ -21,12 +21,31 @@ def sigmoid(X):
     
 
 def softmax(vec):
-    """ Applyies softmax to each row """
+    """ Softmax (for matrix input) """
     #substract max for numerical stability, else for large values will get NaNs due to exp
     #print vec 
     vec1 = np.exp(vec - column_vector(vec.max(axis=1)))
     res = vec1 / column_vector(np.sum(vec1, axis=1))
     return res
 
+def log_softmax(vec):
+    ''' Log of softmax 
+    
+        Due to rounding errors the softmax probability of one values can get to 0. 
+        Taking the log of 0 would generate -inf, so in this function we compute the log together with the softmax
+        to avoid this issue.
+    '''
+    max_vec = max(0.0, np.max(vec))
+    rebased_vec = vec - max_vec
+    return rebased_vec - np.log(np.sum(np.exp(rebased_vec))) 
 
 
+if __name__ == '__main__':
+    print np.log(softmax(np.array([[0,0,100]])))
+    
+    print softmax(np.array([[1, 5,0.2]]))
+    print np.log(softmax(np.array([[1, 5, 0.2]])))
+    print log_softmax(np.array([1, 5,0.2]))
+    
+    print np.logaddexp.reduce([1, 5,0.2])
+    print np.logaddexp(np.logaddexp(1, 5), 0.2)
